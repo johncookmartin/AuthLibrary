458dd1cc018023080c4c29fa2a4da66f060cc197
John Cook-Martin <johncookmartin@gmail.com>
2025-12-06 16:45:44 -0500

    updating google interface to be consistent with service

--- Services/Interfaces/IGoogleService.cs
+++ Services/Interfaces/IGoogleService.cs
@@ -6,7 +6,7 @@ namespace AuthLibrary.Services.Interfaces;
 public interface IGoogleService
 {
     Task<GoogleLoginResult> LoginWithGoogleAsync(GoogleRequestDto googleRequest);
-    string GenerateInitialsFromName(string familyName, string givenName);
+    string GenerateInitialsFromName(string? familyName, string? givenName);
 }
 
 public sealed class GoogleLoginResult
