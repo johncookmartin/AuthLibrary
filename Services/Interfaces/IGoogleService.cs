1b0b9627779b6a1752369d9e28631733a46082b3
John Cook-Martin <johncookmartin@gmail.com>
2025-12-06 16:40:44 -0500

    Refactor so that google references are not required outside of the library.

--- Services/Interfaces/IGoogleService.cs
+++ Services/Interfaces/IGoogleService.cs
@@ -13,10 +13,26 @@ public sealed class GoogleLoginResult
 {
     public bool Succeeded { get; init; }
     public IEnumerable<string>? Errors { get; init; } = Array.Empty<string>();
-    public GoogleJsonWebSignature.Payload? Payload { get; init; }
+    public string? Email { get; init; }
+    public string? Name { get; init; }
+    public string? FamilyName { get; init; }
+    public string? GivenName { get; init; }
+    public string? PictureUrl { get; init; }
+    public string? Subject { get; init; }
+    public bool IsPayloadNull() => Email is null && Name is null && FamilyName is null && GivenName is null && PictureUrl is null && Subject is null;
     public string DisplayName { get; init; } = string.Empty;
     public static GoogleLoginResult Success(GoogleJsonWebSignature.Payload payload, string displayName) =>
-        new() { Succeeded = true, Payload = payload, DisplayName = displayName };
+        new()
+        {
+            Succeeded = true,
+            DisplayName = displayName,
+            Email = payload.Email,
+            Name = payload.Name,
+            FamilyName = payload.FamilyName,
+            GivenName = payload.GivenName,
+            PictureUrl = payload.Picture,
+            Subject = payload.Subject
+        };
     public static GoogleLoginResult Failure(IEnumerable<string> errors) =>
         new()
         {
